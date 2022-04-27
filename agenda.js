95ddcbc6709a16fc22f63c2cd03be1893647b598
Thiago Rocha <102547161+ThRocha7@users.noreply.github.com>
2022-04-26 22:42:04 -0300

    JavaScript, CSS e HTML
    Os arquivos até então.

---
+++ agenda.js
@@ -0,0 +1,16 @@
+var modal = document.getElementById('modal')
+var nome = document.getElementById('txtnome')
+var nasc = document.getElementById('txtdate')
+var tel = document.getElementById('txttel')
+
+var dados
+
+function add(){
+    modal.style.display = 'block'
+
+}
+
+function concluir(){
+    modal.style.display = 'none'
+
+}
